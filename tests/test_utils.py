8df4441e7a36ce15c9184a61cc401ddd354ed40e
Phuker <Phuker@users.noreply.github.com>
2026-06-17 01:19:14 +0800

    test: update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,107 @@ import unittest
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from phuker_music.utils import match_ext_list
+from phuker_music.utils import (
+    assert_,
+    OsPathProxy,
+    match_ext_list,
+)
+
+
+class TestAssert(unittest.TestCase):
+    def test_truthy_no_raise(self):
+        assert_(True)
+        assert_(1)
+        assert_('hello')
+        assert_([1])
+        assert_(object())
+
+    def test_false_raises(self):
+        with self.assertRaises(AssertionError):
+            assert_(False)
+
+    def test_falsy_zero_raises(self):
+        with self.assertRaises(AssertionError):
+            assert_(0)
+
+    def test_falsy_none_raises(self):
+        with self.assertRaises(AssertionError):
+            assert_(None)
+
+    def test_falsy_empty_list_raises(self):
+        with self.assertRaises(AssertionError):
+            assert_([])
+
+    def test_custom_message_raises(self):
+        with self.assertRaisesRegex(AssertionError, 'custom msg'):
+            assert_(False, 'custom msg')
+
+    def test_empty_message_raises(self):
+        with self.assertRaises(AssertionError) as ctx:
+            assert_(False)
+
+        self.assertEqual(str(ctx.exception), '')
+
+
+class TestOsPathProxy(unittest.TestCase):
+    def setUp(self):
+        self.os_path = OsPathProxy()
+
+    def test_abspath_returns_string(self):
+        result = self.os_path.abspath('foo')
+        self.assertIsInstance(result, str)
+        self.assertNotIn('\\', result)
+
+    def test_commonpath_returns_string(self):
+        result = self.os_path.commonpath(['/a/b', '/a/c'])
+        self.assertIsInstance(result, str)
+        self.assertNotIn('\\', result)
+
+    def test_dirname_normalizes(self):
+        result = self.os_path.dirname('a/b/c')
+        self.assertEqual(result, 'a/b')
+
+    def test_expanduser_normalizes(self):
+        result = self.os_path.expanduser('~')
+        self.assertIsInstance(result, str)
+        self.assertNotIn('\\', result)
+
+    def test_join_normalizes_separators(self):
+        result = self.os_path.join('a', 'b')
+        self.assertIsInstance(result, str)
+        self.assertEqual(result, 'a/b')
+
+    def test_normpath_normalizes(self):
+        result = self.os_path.normpath('a//b')
+        self.assertIsInstance(result, str)
+        self.assertNotIn('\\', result)
+
+    def test_realpath_normalizes(self):
+        result = self.os_path.realpath('foo')
+        self.assertIsInstance(result, str)
+        self.assertNotIn('\\', result)
+
+    def test_relpath_normalizes(self):
+        result = self.os_path.relpath('/a/b/c/d', '/a/b')
+        self.assertIsInstance(result, str)
+        self.assertNotIn('\\', result)
+
+    def test_splitext_returns_tuple_untouched(self):
+        result = self.os_path.splitext('foo.txt')
+        self.assertIsInstance(result, tuple)
+        self.assertEqual(result, ('foo', '.txt'))
+
+    def test_exists_returns_bool(self):
+        result = self.os_path.exists('/')
+        self.assertIsInstance(result, bool)
+
+    def test_basename_not_wrapped(self):
+        result = self.os_path.basename('a/b')
+        self.assertEqual(result, 'b')
+
+    def test_isfile_returns_bool(self):
+        result = self.os_path.isfile('/')
+        self.assertIsInstance(result, bool)
 
 
 class TestMatchExtList(unittest.TestCase):
